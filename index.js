dc6bbaf5f9cf529c0ca5aad2b82494db2dd19916
KenRatliff <kjhsfjk@yahoo.com>
2019-11-25 02:26:40 -0600

    Make sure MySQL db is running.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const db = require('./util/database');
 
 const app = express();
 const PORT = 3000;
+
+db.execute('SELECT * FROM participant')
+  .then(result => {
+    console.log(result[0]);
+  }).catch(err => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
