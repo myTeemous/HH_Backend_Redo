c851bf6e0d88d5b6e92e3068a0aeed4b5fda75cd
KenRatliff <kjhsfjk@yahoo.com>
2019-11-30 20:09:46 -0600

    added models folder and Participant class

---
+++ models/participantModel.js
@@ -0,0 +1,8 @@
+module.exports = class Participant {
+    constructor(email, firstName, lastName, school) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.school = school;
+    }
+}
