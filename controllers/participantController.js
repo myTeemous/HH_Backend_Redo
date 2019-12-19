f87e98b0a7a647d7ba9640db70d270aabd7548ce
KenRatliff <kjhsfjk@yahoo.com>
2019-12-18 19:49:12 -0600

    Made a new database. Made small changes to getParticipant function

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -9,13 +9,14 @@ const saveParticipant = async (req, res) => {
 const getParticipant = async (req, res) => {
     try {
         const email = req.body.email;
-        const response = await pool.query('SELECT * FROM public."PARTICIPANT" WHERE email = $1', [email]);
+        const response = await pool.query('SELECT * FROM participant WHERE email = $1', [email]);
         console.log(response.rows[0]);
         res.end();
         //res.status(200).json(response.row[0]);
     }
     catch (err) {
         console.log(err);
+        res.end();
     }
 };
 
--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'hackhouston',
-    password: 'Q!squirestrat35',
+    database: 'hackhoustondb',
+    password: '',
     port: 5432
 });
 
