6ba5b87d777ad6d32bdc60f2459d56f470a1bdc5
KenRatliff <kjhsfjk@yahoo.com>
2019-12-23 02:40:17 -0600

    Added comments to the participant controller.

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -3,14 +3,19 @@ const pool = require('../util/database');  //needed for the database connection
 //save an individual participant to the database
 const saveParticipant = async (req, res) => {
     try {
+        //Before inserting a new participant, check and see if their school exists.
         const school = req.body.school.toUpperCase();
         const response = await pool.query('SELECT id FROM school WHERE school_name = $1', [school]);
 
+        //If the participants school exists, get the school id and insert it into the participant
+        //table along with their first name, last name, and email.
         if(response.rows.length > 0) {
             const response2 = await pool.query('INSERT INTO participant (first_name, last_name, email, school_id) VALUES ($1, $2, $3, $4) RETURNING id',
             [req.body.firstName, req.body.lastName, req.body.email, parseInt(response.rows[0].id)]);
             res.status(200).json({ registrationConfirmation: 'You have successfully registered for HackHouston!' });
         }
+        //If the participants school does not exist, insert the school first, then insert the
+        //participants information.
         else {
             const response2 = await pool.query('INSERT INTO school (school_name) VALUES ($1) RETURNING id', [school]);
             const response3 = await pool.query('INSERT INTO participant (first_name, last_name, email, school_id) VALUES ($1, $2, $3, $4)',
@@ -24,7 +29,7 @@ const saveParticipant = async (req, res) => {
     }
 };
 
-//retrieve all information on an individual participant
+//Get the name, email, and school of an individual participant.
 const getParticipant = async (req, res) => {
     try {
         const email = req.body.email;
@@ -37,7 +42,7 @@ const getParticipant = async (req, res) => {
     }
 };
 
-//retrieve all participants
+//Get the name, email, and school of all participants.
 const getAllParticipants = async (req, res) => {
         
 };
