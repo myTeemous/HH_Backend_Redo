f664c4e377724dd3251011b50d20d108e265e3df
KenRatliff <kjhsfjk@yahoo.com>
2020-01-03 00:02:57 -0600

    Session setup with local store.

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
 const PORT = 3000;
+const sessionLifeTime = 1000 * 60 * 5;
 
 //import routes
 const registerRoutes = require('./routes/register');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'jfe9ru79&Jkdoqpjr20rF#@dr',
+    cookie: {
+        maxAge: sessionLifeTime,
+        sameSite: true,
+    }
+}));
 
 //mount routes
 app.use(registerRoutes);
